d84484d139bfb25ba54e5f1e9404198dbe16b2ff
ajfisher <ajfisher.td@gmail.com>
2018-12-27 16:17:37 +1100

    Added dev tooling files

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+// const nodeExternals = require('webpack-node-externals');
+
+
+module.exports = {
+  context: __dirname + '/src',
+
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: [/node_modules/],
+      use: [{
+        loader: 'babel-loader',
+        options: { presets: ['env'] }
+      }]
+    }]
+  },
+
+  entry: {
+    app: './app.js'
+  },
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist', 'assets'),
+    publicPath: '/assets'
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src')
+  },
+  devtool: 'source-map',
+
+  node: {
+    fs: 'empty',
+    module: 'empty'
+  }
+
+  // target: 'node',
+  // externals: [nodeExternals()],
+};
